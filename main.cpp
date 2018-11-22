702018af3d4b9e057f635e1a5a2639072adc67bd
Gaetan et Remy <adgaetan.mf@outlook.fr>
2018-11-22 12:04:19 +0100

    Description des auteurs

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+//Programme d'Antoine Laroye (aka Laroye Arthur), Thomas Barthélémy (aka Bartoche), Rémy Maige (aka Retho) et Gaétan Moisson-Franckhauser (aka CubicOtter)
 #include <iostream>
 
 using namespace std;
