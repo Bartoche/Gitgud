56e383db4b6490d75a1d2f3a8a852910cedcedd9
Thomas <thomas.barthelemy@eleves.ec-nantes.fr>
2018-11-22 12:23:06 +0100

    lul

--- main.cpp
+++ main.cpp
@@ -7,5 +7,6 @@ int main()
   cout<<"Salut c'est cool"<<endl;
   cout<<"Thomas est très beau"<<endl;
   cout<<"Éditer à plusieurs, quelle réussite !"<<endl;
+  cout<<"J'avoue"<< endl;
   return 0;
 }
